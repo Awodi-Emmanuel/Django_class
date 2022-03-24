b61748d86d372c231a2456ceb02772a048b05bfe
Awodi-Emmanuel <Emmanuelusman701@gmail.com>
2022-03-24 19:14:01 +0100

    completed urlspth

---
+++ feed/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.forms import CharField
+
+# Create your models here.
+class Post(models.Model):
+  text = models.CharField(max_length=178, blank=False)
+
+  def __str__(self):
+    return self.text 
---
+++ feed/urls.py
@@ -0,0 +1,8 @@
+from cgitb import Hook
+from django.urls import path
+from .views import HomePageView 
+app_name = 'feed'
+
+urlpatterns = [
+  path('', HomePageView.as_view(), name='index'),
+]
