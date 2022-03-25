5f49f5673d4330e02a93ef00a819b4c87b6569bf
Awodi-Emmanuel <Emmanuelusman701@gmail.com>
2022-03-25 13:31:10 +0100

    dynamic content

--- feed/models.py
+++ feed/models.py
@@ -3,7 +3,7 @@ from django.forms import CharField
 
 # Create your models here.
 class Post(models.Model):
-  text = models.CharField(max_length=178, blank=False)
+  text = models.CharField(max_length=178, blank=False, null=False)
 
   def __str__(self):
     return self.text 
